98db4d013776c106083e7f38dffa9611868d28d4
Maddie <115709272+Maddily@users.noreply.github.com>
2022-12-27 13:57:42 +0200

    Return 'Rock' if the first condition is met

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 100) - 1;
     //Create an if statement with a condition to check if the random number is between 0 and 32
     if (randomNum >= 0 && randomNum <= 32) {
-        
+        //Return 'Rock' if the condition is met
+        return 'Rock';
     }
 }
