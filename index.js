c3ec1b3ee0640d247aeb14b36d6cadcedb57459b
Maddie <115709272+Maddily@users.noreply.github.com>
2022-12-27 13:56:26 +0200

    Create an if statement
    The if statement checks if the random number is between 0 and 32

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
 function getComputerChoice() {
     //Declare a variable inside the function and let it contain a generated random number
     let randomNum = Math.floor(Math.random() * 100) - 1;
+    //Create an if statement with a condition to check if the random number is between 0 and 32
+    if (randomNum >= 0 && randomNum <= 32) {
+        
+    }
 }
