11def1bb63b35878b44eb2109a8a7a5fa82f1cf2
Maddie <115709272+Maddily@users.noreply.github.com>
2022-12-27 14:43:23 +0200

    Return a string based on a condition
    If the user's selection is 'scissors' and the computer's selection is 'paper', return "You win! Scissors beats Paper"

--- index.js
+++ index.js
@@ -36,4 +36,8 @@ function playRound(playerSelection, computerSelection) {
     else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'scissors') {
         return "You lose! Scissors beats Paper";
     }
+    //If the user's selection is 'scissors' and the computer's selection is 'paper', return "You win! Scissors beats Paper"
+    else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'paper') {
+        return "You win! Scissors beats Paper";
+    }
 }
