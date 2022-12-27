1c430a282d259e314e26b60ca8f1694ab84bca3a
Maddie <115709272+Maddily@users.noreply.github.com>
2022-12-27 19:11:37 +0200

    Remove toLowerCase method from computerSelection

--- index.js
+++ index.js
@@ -5,47 +5,47 @@ function getComputerChoice() {
     //Create an if statement with a condition to check if the random number is between 0 and 32
     if (randomNum >= 0 && randomNum <= 32) {
         //Return 'Rock' if the condition is met
-        return 'Rock';
+        return 'rock';
     }
     //If the condition isn't met, add another condition to check if the random number is between 33 and 65
     else if (randomNum >= 33 && randomNum <= 65) {
         //Return 'Paper' if the condition is met
-        return 'Paper';
+        return 'paper';
     }
     //If the conditions aren't met, return 'Scissors'
     else {
-        return 'Scissors';
+        return 'scissors';
     }
 }
 
 //Create a function that takes two parameters and plays one round of Rock Paper Scissors
 function playRound(playerSelection, computerSelection) {
     //If the player's selection is 'rock' and the computer's selection is 'scissors', return "You win! Rock beats Scissors"
-    if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'scissors') {
+    if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
         return "You win! Rock beats Scissors";
     }
     //If the player's selection is 'rock' and the computer's selection is 'paper', return "You lose! Paper beats Rock"
-    else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'paper') {
+    else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper') {
         return "You lose! Paper beats Rock";
     }
     //If the player's selection is 'paper' and the computer's selection is 'rock', return "You win! Paper beats Rock"
-    else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'rock') {
+    else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
         return "You win! Paper beats Rock";
     }
     //If the player's selection is 'paper' and the computer's selection is 'scissors', return "You lose! Scissors beats Paper"
-    else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'scissors') {
+    else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
         return "You lose! Scissors beats Paper";
     }
     //If the player's selection is 'scissors' and the computer's selection is 'paper', return "You win! Scissors beats Paper"
-    else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'paper') {
+    else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
         return "You win! Scissors beats Paper";
     }
     //If the player's selection is 'scissors' and the computer's selection is 'rock', return "You lose! Rock beats Scissors"
-    else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'rock') {
+    else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock') {
         return "You lose! Rock beats Scissors";
     }
     //if the player's selection equals the computer's selection, return "It's a tie!"
-    else if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+    else if (playerSelection.toLowerCase() == computerSelection) {
         return "It's a tie!";
     }
     //If the user's input isn't rock, paper or scissors, return "Wrong input! check your spelling"
@@ -53,3 +53,10 @@ function playRound(playerSelection, computerSelection) {
         return "Wrong input! check your spelling";
     }
 }
+
+const computerSelection = getComputerChoice();
+const playerSelection = 'rock';
+
+console.log(getComputerChoice());
+console.log(playerSelection);
+console.log(playRound(playerSelection, computerSelection));
