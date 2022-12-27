e37a8844f6e68c01440cdac9d43eb82d4dc2ded8
Maddie <115709272+Maddily@users.noreply.github.com>
2022-12-27 13:55:09 +0200

    Declare a variable inside the function
    The variable contains a generated random number

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //Create a function that randomly returns 'Rock', 'Paper' or 'Scissors'
 function getComputerChoice() {
-    
+    //Declare a variable inside the function and let it contain a generated random number
+    let randomNum = Math.floor(Math.random() * 100) - 1;
 }
